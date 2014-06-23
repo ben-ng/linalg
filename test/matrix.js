ecc03a8c4ecdd8e96ed65de4d5f8b087cee66d1e
Ben Ng <me@benng.me>
2014-06-23 01:32:33 -0700

    Leave reductions to the wrapper

--- test/matrix.js
+++ test/matrix.js
@@ -3,15 +3,19 @@ var matrix = require('../lib/matrix')
 
 assert.throws(function () {
   matrix.dot([[1]], [1, 2])
-}, /Dot product requires matrix.columns = vector\.dimensions/)
+}, /Multiplying \(matrix \. vector\) requires matrix.columns = vector\.dimensions/)
 
 assert.throws(function () {
   matrix.dot([1, 2], [[1]])
-}, /Dot products of the form \(vector \. matrix\) are not possible/)
+}, /Multiplications of the form \(vector \. matrix\) are not possible/)
 
 assert.throws(function () {
   matrix.dot([[1], [3], [5]], [[1, 2], [4, 5]])
-}, /Dot product requires two matrices a, b where a\.columns = b\.rows/)
+}, /Matrix multiplication requires matrices a, b where a\.columns = b\.rows/)
+
+assert.deepEqual(matrix.diagonalMatrix([1, 2]), [[1, 0], [0, 2]], 'diagonalMatrix should work with array')
+assert.deepEqual(matrix.diagonalMatrix(1, 2), [[1, 0], [0, 2]], 'diagonalMatrix should work with multiple args')
+assert.deepEqual(matrix.identity(3), matrix.diagonalMatrix(1, 1, 1), 'identityMatrix should be a diagonalMatrix')
 
 assert.deepEqual(matrix.dot(
   [
@@ -27,26 +31,6 @@ assert.deepEqual(matrix.dot(
     [58, 64]
   , [139, 154]
   ], 'matrix dot matrix should work')
-
-assert.deepEqual(matrix.dot(
-  [
-    [1, 2, 3]
-  , [4, 5, 6]
-  ]
-, [
-    [7, 8]
-  , [9, 10]
-  , [11, 12]
-  ]
-, matrix.identity(2)
-, [
-    [2, 4]
-  , [6, 8]
-  ])
-, [
-    [58 * 2 + 64 * 6, 58 * 4 + 64 * 8]
-  , [139 * 2 + 154 * 6, 139 * 4 + 154 * 8]
-  ], 'chaining matrix multiplication should work')
 
 assert.deepEqual(matrix.transpose([[2, 3], [4, 5]])
   , [[2, 4], [3, 5]]
